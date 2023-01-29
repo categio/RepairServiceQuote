006f7b4d9d3fdbfd68ee052d1568c792be7c3b44
Categio <categio1@gmail.com>
2023-01-28 17:45:19 -0800

    starting point for project that I will finish at a later date

--- Car.cs
+++ Car.cs
@@ -6,5 +6,37 @@ namespace RepairServiceQuote
 {
     class Car
     {
+        //Fields to create class Car objs
+        private string _make;
+        private string _model;
+        private int _year;
+        
+        //Constructor for class Car
+        public Car(string make, string model, int year)
+        {
+            _make = make;
+            _model = model;
+            _year = year;
+        }
+
+        //make property getter+setter
+        public string Make
+        {
+            get { return _make; }
+            set { _make = value; }
+        }
+
+        //model property getter+setter
+        public string Model
+        {
+            get { return _model; }
+            set { _model = value; }
+        }
+        //year property getter+setter
+        public int Year
+        {
+            get { return _year; }
+            set { _year = value; }
+        }
     }
 }
--- ServiceQuote.cs
+++ ServiceQuote.cs
@@ -6,5 +6,44 @@ namespace RepairServiceQuote
 {
     class ServiceQuote
     {
+        //Fields to build a ServiceQuote obj
+        private decimal _partsCharges;
+        private decimal _laborCharges;
+        private decimal _taxRate;
+
+        public ServiceQuote(decimal partsCharges, decimal laborCharges, decimal taxRate)
+        {
+            _partsCharges = partsCharges;
+            _laborCharges = laborCharges;
+            _taxRate = taxRate;
+        }
+        //PartsCharges property gettter+setter
+        public decimal PartsCharges
+        {
+            get { return _partsCharges ; }
+            set { _partsCharges = value; }
+        }
+        //PartsCharges property gettter+setter
+        public decimal laborCharges
+        {
+            get { return _laborCharges; }
+            set { _laborCharges = value; }
+        }
+        //PartsCharges property gettter+setter
+        public decimal taxRate
+        {
+            get { return _taxRate; }
+            set { _taxRate = value; }
+        }
+        //SalesTax property (read-only)
+        public decimal SalesTax
+        {
+            get { return _partsCharges* _taxRate; }
+        }
+        //Total charges propert (read-only)
+        public decimal TotalCharges
+        {
+            get { return _partsCharges + _laborCharges + (_partsCharges * _taxRate); }
+        }
     }
 }
